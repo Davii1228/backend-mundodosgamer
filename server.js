539e9f0482b1c9e6bd7aeb5f5a31dbda1b1fd4b9
Davii1228 <adrielepinheiro449@gmail.com>
2026-02-21 02:43:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,23 +7,6 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API PIX ONLINE 🚀");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
-});
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
   res.send("API ONLINE 🚀");
 });
 
